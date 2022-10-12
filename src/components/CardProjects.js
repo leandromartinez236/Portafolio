7ab3b06a89df272f6aad44efa63a7744d449b784
leandro <martinezleandro2004@gmail.com>
2022-10-12 15:41:50 -0300

    new cardsProjects

--- src/components/CardProjects.js
+++ src/components/CardProjects.js
@@ -1,22 +1,48 @@
 import React from "react";
-import { Box, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/images/flutter.png";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/images/wheaterapp.jpeg";
 
 const CardProjects = ({ text }) => {
+
   return (
-    <Box bgColor="red.400" p='15px' w="220px" h='350px' maxH='350px'>
-      <Image w="100%" objectFit='contain'
-        // h="80px" 
-        h='38%'
-        src={logo} />
-      <Text
-        //  maxH='170px' 
-        pt='15px'
-        maxH='60%'
-        overflow='hidden'
-        textOverflow='clip'>
-        {text}
-      </Text>
+    <Box
+      m='0 auto'
+      maxW={["220px", "300px", "330px", "360px"]}
+      transition="all 500ms ease"
+      borderTopRadius='15px'
+      _hover={{
+        transform: "scale(1.04)",
+        transition: "all 500ms ease",
+        boxShadow: "2px 2px 7px 1px gray",
+      }}
+      borderBottomRadius='15px'
+    >
+      <Box>
+        <Image
+          borderTopRadius='10px'
+          objectFit='contain' bgRepeat='no-repeat' bgPos='top left' height={"100%"} m="0 auto" src={logo}
+        />
+      </Box>
+      <Flex borderBottomRadius='15px' p="1.2rem" bgColor="#009688d4" flexDirection={"column"}>
+        <Heading
+          fontSize={18}
+          fontWeight={700}
+          color='#fff'
+          mb="12px"
+          overflow="hidden"
+        >
+          Wheater App
+        </Heading>
+        <Text
+          fontSize={16}
+          fontWeight={400}
+          color='#fff'
+          h="100px"
+          overflow="hidden"
+        >
+          {text}
+        </Text>
+      </Flex>
     </Box>
   );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,22 +1,35 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Grid, Heading } from "@chakra-ui/react";
 import React from "react";
+import { useSelector } from "react-redux";
 import CardProjects from "../components/CardProjects";
+import { selectMode } from "../features/theme";
 
 const Projects = () => {
+  const darkMode = useSelector(selectMode)
   const pr =
     "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Sed, nostrum doloremque impedit libero animi neque iste delectus cum! In nobis voluptatem rerum quod fugiat voluptate ex quas ea fugit saepe nemo, dicta eveniet sunt sint eius id optio vel asperiores reiciendis aperiam alias autem tempora dolores impedit.Provident, ex nulla ?";
   const p2 =
     "Lorem iluptate ex quas ea fugit saepe nemo, dicta eveniet sunt ?";
   return (
-    <Box bgColor="gray">
-      <Heading fontSize={30} textAlign="center" color="#000">
-        Projects
-      </Heading>
-      <Flex>
+    <Box
+      bgColor={darkMode ? '#1b1717' : '#fff'}
+      borderTop={1}>
+      <Flex justifyContent='center' flexDir='column' align='center'>
+        <Heading fontSize={30} textAlign="center" color="#000">
+          Projects
+        </Heading>
+        <Box width='40px' h='2px' border={1} bgColor={darkMode ? 'orange' : '#4479ed'}></Box>
+      </Flex>
+      <Grid
+        maxW='90%'
+        m='0 auto'
+        gridTemplateColumns="repeat(auto-fit, minmax(230px,1fr))"
+        gap={["2rem", "2rem", "2rem", "4rem"]}
+      >
         <CardProjects text={pr} />
         <CardProjects text={p2} />
-        {/* <CardProjects /> */}
-      </Flex>
+        <CardProjects text={pr} />
+      </Grid>
     </Box>
   );
 };
