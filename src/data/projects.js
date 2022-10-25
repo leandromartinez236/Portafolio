e37dbe7cd25e5b551c85a3157b73b989b0a3c077
leandro <martinezleandro2004@gmail.com>
2022-10-25 13:18:17 -0300

    feat: data projects modified

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,25 +1,56 @@
-import wheaterApp from '../assets/images/wheaterapp.jpeg'
-import myfinances from '../assets/images/finances.jpeg'
-import tiendaMueble from '../assets/images/tiendamueble.jpeg'
-import morty from '../assets/images/rickAndMorty.jpeg'
+import wheaterApp from "../assets/images/wheaterapp.jpeg";
+import myfinances from "../assets/images/finances.jpeg";
+import tiendaMueble from "../assets/images/tiendamueble.jpeg";
+import morty from "../assets/images/rickAndMorty.jpeg";
 
 export const dataProjects = [
   {
-    id: '1', github: 'https://github.com/leandromartinez236/My-finances-App', web: false, image: myfinances, title: 'My finances', text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Sed, nostrum doloremque impedit libero animi neque iste delectus cum! In nobis voluptatem rerum quod fugiat voluptate ex quas ea fugit saepe nemo, dicta eveniet sunt sint eius id optio vel asperiores reiciendis aperiam alias autem tempora dolores impedit.Provident, ex nulla ?"
+    id: "1",
+    github: "https://github.com/leandromartinez236/My-finances-App",
+    web: false,
+    image: myfinances,
+    title: "My finances",
+    text: "Aplicación diseñada para organizar tus gastos e ingresos, registrar con fecha tus movimientos de dinero y poder revisar los totales, mediante un gráfico de pastel.",
   },
   {
-    id: '2', github: 'https://github.com/leandromartinez236/Wheater-APP', web: 'https://wheater-app-pearl.vercel.app/', image: wheaterApp, title: 'WheaterApp', text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Sed, nostrum doloremque impedit libero animi neque iste delectus cum! In nobis voluptatem rerum quod fugiat voluptate ex quas ea fugit saepe nemo, dicta eveniet sunt sint eius id optio vel asperiores reiciendis aperiam alias autem tempora dolores impedit.Provident, ex nulla ?"
+    id: "2",
+    github: "https://github.com/leandromartinez236/Wheater-APP",
+    web: "https://wheater-app-pearl.vercel.app/",
+    image: wheaterApp,
+    title: "WheaterApp",
+    text: "Ofrece información climática sobre la ciudad que elijas, con su respectiva temperatura (min°, max°), humedad, viento.",
   },
   {
-    id: '3', github: 'https://github.com/leandromartinez236/tienda-mueble', web: 'https://tienda-mueble-pink.vercel.app/', image: tiendaMueble, title: 'Tienda Mueble', text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Sed, nostrum doloremque impedit libero animi neque iste delectus cum! In nobis voluptatem rerum quod fugiat voluptate ex quas ea fugit saepe nemo, dicta eveniet sunt sint eius id optio vel asperiores reiciendis aperiam alias autem tempora dolores impedit.Provident, ex nulla ?"
+    id: "3",
+    github: "https://github.com/leandromartinez236/tienda-mueble",
+    web: "https://tienda-mueble-pink.vercel.app/",
+    image: tiendaMueble,
+    title: "Tienda Mueble",
+    text: "Página web para la venta de muebles, cuenta con un hero, seccion categorias y variedad de productos con responsive design.",
   },
   {
-    id: '4', github: 'https://github.com/leandromartinez236/rick-and-morty-select', web: 'https://rick-and-morty-umber-eight.vercel.app/', image: morty, title: 'Rick and Morty API', text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Sed, nostrum doloremque impedit libero animi neque iste delectus cum! In nobis voluptatem rerum quod fugiat voluptate ex quas ea fugit saepe nemo, dicta eveniet sunt sint eius id optio vel asperiores reiciendis aperiam alias autem tempora dolores impedit.Provident, ex nulla ?"
+    id: "4",
+    github: "https://github.com/leandromartinez236/rick-and-morty-select",
+    web: "https://rick-and-morty-umber-eight.vercel.app/",
+    image: morty,
+    title: "Rick and Morty API",
+    text: "Elige a tu personaje favorito de Rick and morty y consigue toda su información en formato de tarjetas llamativas.",
   },
   {
-    id: '5', github: 'https://github.com/leandromartinez236/restapi-sequelize', web: false, image: false, title: 'Sequelize API', text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Sed, nostrum doloremque impedit libero animi neque iste delectus cum! In nobis voluptatem rerum quod fugiat voluptate ex quas ea fugit saepe nemo, dicta eveniet sunt sint eius id optio vel asperiores reiciendis aperiam alias autem tempora dolores impedit.Provident, ex nulla ?"
+    id: "5",
+    github: "https://github.com/leandromartinez236/restapi-sequelize",
+    web: false,
+    image: false,
+    title: "API con Sequelize",
+    text: "API completa con su respectivo C.R.U.D para las tareas y proyectos, creacion de modelos, controladores y rutas.",
   },
   {
-    id: '6', github: 'https://github.com/leandromartinez236/API-REST-with-TypeScript-Express-y-NodeJS', web: false, image: false, title: 'API con Typescript', text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Sed, nostrum doloremque impedit libero animi neque iste delectus cum! In nobis voluptatem rerum quod fugiat voluptate ex quas ea fugit saepe nemo, dicta eveniet sunt sint eius id optio vel asperiores reiciendis aperiam alias autem tempora dolores impedit.Provident, ex nulla ?"
+    id: "6",
+    github:
+      "https://github.com/leandromartinez236/API-REST-with-TypeScript-Express-y-NodeJS",
+    web: false,
+    image: false,
+    title: "API con Typescript",
+    text: "Podrás ver y crear reviews de peliculas, colocando calificación y una breve reseña",
   },
-]
+];
