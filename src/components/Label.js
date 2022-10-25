47c95e70decf71c3ec97008e135d4227396a386c
leandro <martinezleandro2004@gmail.com>
2022-10-25 13:47:33 -0300

    feat: label created

---
+++ src/components/Label.js
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Label = ({ text }) => {
+  return (
+    <Box>
+      <Text>{text}</Text>
+    </Box>
+  )
+}
+
+export default Label
